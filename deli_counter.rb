f7e10cc1d3d1bed1b71041ccf6260b75f6a3615a
meggs1 <megan.schertz@gmail.com>
2020-10-23 18:18:31 -0500

    commit tree 396a90da38307cc585f6d0755955d3cc97272b4f

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+require "pry"
+
+def line(array)
+    if array.length == 0
+        puts "The line is currently empty."
+     else
+        i_name = ""  # empty string for name and index value
+        array.each_with_index do |name, i|
+            i = i + 1
+         i_name << " #{i}. #{name}" # adding index and name interlopation to empty value
+        end
+      puts "The line is currently:#{i_name}"  
+    end
+end
+
+def take_a_number(array, name)
+    array.push(name)  #adds name values to the end of the array
+    if array.length > 0
+        puts "Welcome, #{name}. You are number #{array.length} in line."
+    end    
+end
+
+def now_serving(array)
+    if array.length == 0
+        puts "There is nobody waiting to be served!"
+    else        
+        puts "Currently serving #{array[0]}." #first value in array
+        array.shift        #deleting first value
+    end
+end
